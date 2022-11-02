c716b8906e203efc8519872eaf0e2604a2a5be2f
gabpinochet <117171907+gabpinochet@users.noreply.github.com>
2022-11-03 00:55:15 +0100

    Subida lección 2

---
+++ Curso Javascript/2. Sintaxis variables y palabras reservadas/Variables.js
@@ -0,0 +1,12 @@
+const fecha = new Date(1991, 6, 3);
+
+const libro = {
+    titulo: "Fullmetal Alchemist",
+    autor: "Hiromu Arakawa",
+    fecha: 2001,
+    url: "https://www.amazon.es/Fullmetal-alchemist-kanzenban-C%C3%93MIC-MANGA/dp/8467913134/"
+}
+
+const perfil = ["Gabriel", 31, true, fecha, libro];
+
+console.log(perfil);
