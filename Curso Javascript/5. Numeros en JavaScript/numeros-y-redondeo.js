3cf40556d8c4bebb6132f38d387903d54b507293
gabpinochet <117171907+gabpinochet@users.noreply.github.com>
2022-11-09 12:11:55 +0100

    Subida tema 5

---
+++ Curso Javascript/5. Numeros en JavaScript/numeros-y-redondeo.js
@@ -0,0 +1,10 @@
+let altura_cm = 172
+let altura_m = 1.72
+let peso_kg = 85.60;
+let altura_red = altura_m.toFixed(0);
+let peso_red = Math.floor(peso_kg);
+let is_max = ((Number.MAX_VALUE + 1) == Number.MAX_VALUE)
+
+console.log(altura_red)
+console.log(peso_red)
+console.log(is_max)
