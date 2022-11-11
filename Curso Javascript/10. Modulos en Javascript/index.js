17e16a8151693f08340292ee7f97df12035e8314
gabpinochet <117171907+gabpinochet@users.noreply.github.com>
2022-11-11 14:14:19 +0100

    Subida tema 10

---
+++ Curso Javascript/10. Modulos en Javascript/index.js
@@ -0,0 +1,8 @@
+import {suma, multiplica} from "./controller.js"
+import chalk from 'chalk';
+
+const suma1 = suma(1,2)
+const suma2 = suma(4,5)
+const multiplicacion = multiplica(suma1,suma2)
+
+console.log(chalk.green(multiplicacion))
