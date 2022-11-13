c0a5a3315c8a84cb1b2bdd1ef180a6e2d43f15fb
gabpinochet <117171907+gabpinochet@users.noreply.github.com>
2022-11-13 15:18:34 +0100

    Subida tema 14

---
+++ Curso Javascript/14. Gestion de eventos/index.js
@@ -0,0 +1,11 @@
+const boton = document.querySelector("#btn")
+
+boton.addEventListener("click", () => {
+    alert("Click en el botón")
+})
+
+$(() => {
+    $("#btn").click(() => {
+        console.log("Hola, estoy utilizando jQuery")
+    })
+})
